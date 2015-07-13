ec771b598b11e3b75b1775feb23ae72f1a056c15
root <root@paradox.(none)>
2015-07-13 15:36:59 +0200

    let to exit

--- server.py
+++ server.py
@@ -6,16 +6,27 @@ import re
 global s
 s = 0
 
+global exit_flag
+exit_flag = False
+
 def do_motor():
     #global s
     #print s
     #s +=1
-    threading.Thread(target = do_motor).start()
-
+    if not exit_flag:
+        threading.Thread(target = do_motor).start()
+    
 def parse_data(raw_data):
     m = re.search('\?(.+?) ', raw_data)
     if m:
-        return m.group(1)
+        tmp_params = m.group(1).split('&')
+        ret_dic = {}
+        for item in tmp_params:
+            var_tmp = item.split('=')
+            ret_dic[var_tmp[0]]=var_tmp[1]
+
+        return ret_dic
+
     else:
         return None
 
@@ -40,11 +51,22 @@ while True:
 
         # Receive the data in small chunks and retransmit it
     data = connection.recv(1024)
-    if data:
-        print >>sys.stderr, 'received --->"%s"' % parse_data(data)
+    data_dic = parse_data(data)
+    
     
     connection.sendall('Hello %s' %a)
     connection.close()
-    a += 1   
+    a += 1
+    if data_dic:
+        print >>sys.stderr, 'received --->"%s"' % data_dic
+        if 'x' in data_dic:
+            print "exiting"
+            #threading.Thread(target = do_motor).stop()
+            exit_flag = True
+            sock.close()
+            exit(1)
+            
+
+
 
 
